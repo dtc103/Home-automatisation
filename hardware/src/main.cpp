06d69e9e3faa280b9c165ab9c2d7be5fe2baf912
dtc103 <jan_kerner@gmx.de>
2022-03-23 19:51:58 +0100

    .

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -4,13 +4,13 @@
 
 PanelServer server(80);
 
-char ssid[] = "FRITZ!Box 6490 Cable";
-char pw[] = "97445628199902922569";
+char ssid[] = "devicenetwork";
+char pw[] = "923492760463546739763524";
 
 void setup() {
     Serial.begin(9600);
 	server.begin(ssid, pw);
-    server.authenticate(ACCESS_POINT_NAME, ACCESS_POINT_PORT, DEVICE_NAME);
+    //server.authenticate(ACCESS_POINT_NAME, ACCESS_POINT_PORT, DEVICE_NAME);
 }
 
 void loop() {
