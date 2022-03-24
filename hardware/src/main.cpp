a60b6b9ffa12402abfec23d65e1ea47af8473787
dtc103 <jan_kerner@gmx.de>
2022-03-24 15:08:04 +0100

    renamed rocket.toml file

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -2,10 +2,17 @@
 #include <PanelServer.h>
 #include <PanelDefinitions.h>
 
+//#define CHECK_AP 1
+
 PanelServer server(80);
 
+#ifdef CHECK_AP
 char ssid[] = "devicenetwork";
 char pw[] = "6C-ji40a";
+#else
+char ssid[] = "FRITZ!Box 6490 Cable";
+char pw[] = "97445628199902922569";
+#endif
 
 void setup() {
     Serial.begin(9600);
